64e07a80305d88e3519c1e8dc7907ec875506e06
Your Name <you@example.com>
2024-04-30 16:50:00 -0700

    test dag

--- dags/baseball_etl.py
+++ dags/baseball_etl.py
@@ -4,10 +4,14 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from baseball.baseball import run_baseball_stats
 
+def test():
+    print('Hello World')
+    return
+
 
 with DAG("baseball", start_date = datetime(2024,4,29), schedule_interval = "@daily", catchup=False) as dag:
     datarun = PythonOperator(
         task_id = "fullrun",
-        python_callable = run_baseball_stats,
+        python_callable = test,
         dag=dag)    
     
