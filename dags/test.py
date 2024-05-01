15d987c5bb70ea59a3fe6344aeb5159516e489b1
Your Name <you@example.com>
2024-05-01 13:00:26 -0700

    tesing db connec

--- dags/test.py
+++ dags/test.py
@@ -1,7 +1,7 @@
 
 
 from datetime import datetime, timedelta
-
+from db_util
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
@@ -9,9 +9,28 @@ def tester():
     print('hello world')
     return
 
+
+def readSnowflake():
+
+    my_db = db_connect(dag=DAG, conn_type='snowflake', conn_id='snowflake_ods_staging')
+    sql = '''
+                   select *
+                   from ods.curr_items
+                   limit 199
+                   '''
+    my_db.db_execute(sql=sql)
+    rows = my_db.db_getAllRows()
+    for row in rows:
+        print(row)
+
+      
 with DAG("baseball", start_date = datetime(2024,4,29), schedule_interval = "@daily", catchup=False) as dag:
     datarun = PythonOperator(
         task_id = "fullrun",
         python_callable = tester,
         dag=dag)    
+    
+    read = PythonOperator(dag=dag,
+                          task_id="readSnowflake",
+                          python_callable=readSnowflake)
     
