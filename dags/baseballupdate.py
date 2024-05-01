638ac649bfcec98e3d825157a92eff89342df62f
Your Name <you@example.com>
2024-05-01 15:28:50 -0700

    das

--- dags/baseballupdate.py
+++ dags/baseballupdate.py
@@ -8,6 +8,6 @@ from airflow.operators.python_operator import PythonOperator
 with DAG("baseball", start_date = datetime(2024,4,29), schedule_interval = "@daily", catchup=False) as dag:
     datarun = PythonOperator(
         task_id = "updatesheet",
-        python_callable = updatstats,
+        python_callable = updatestats,
         dag=dag)    
     
