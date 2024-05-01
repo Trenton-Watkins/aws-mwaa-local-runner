524715b8bf9b31c7c81f99d398c5e2e37ce7183b
Your Name <you@example.com>
2024-05-01 12:09:37 -0700

    fasdff

--- dags/test.py
+++ dags/test.py
@@ -5,11 +5,13 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-
+def tester():
+    print('hello world')
+    return
 
 with DAG("baseball", start_date = datetime(2024,4,29), schedule_interval = "@daily", catchup=False) as dag:
     datarun = PythonOperator(
         task_id = "fullrun",
-        python_callable = run_baseball_stats,
+        python_callable = tester,
         dag=dag)    
     
